da6a758f759c45d74a010d1e7d96f00590081f2d
pkaho <pkaho@qq.com>
2024-06-03 17:50:13 +0800

    venv-selector switch regexg

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,13 +1,13 @@
 return {
   -- project manager
   {
-    'ahmedkhalf/project.nvim',
-    event = 'VeryLazy',
+    "ahmedkhalf/project.nvim",
+    event = "VeryLazy",
     keys = {
-      { '<leader>P', "<cmd>lua require('telescope').extensions.projects.projects()<cr>", desc = 'Projects Manager' },
+      { "<leader>P", "<cmd>lua require('telescope').extensions.projects.projects()<cr>", desc = "Projects Manager" },
     },
     config = function()
-      require('project_nvim').setup({
+      require("project_nvim").setup({
         active = true,
         on_config_done = nil,
         manual_mode = false,
@@ -19,36 +19,36 @@ return {
         silent_chdir = true,
         scope_chdir = "global",
       })
-    end
+    end,
   },
 
   -- improved fzf.vim written in lua
   {
-    'ibhagwan/fzf-lua',
+    "ibhagwan/fzf-lua",
     dependencies = {
-      'nvim-tree/nvim-web-devicons',
+      "nvim-tree/nvim-web-devicons",
     },
     opts = {},
   },
 
   -- git
   {
-    'neogitorg/neogit',
-    event = 'VeryLazy',
+    "neogitorg/neogit",
+    event = "VeryLazy",
     dependencies = {
-      'nvim-lua/plenary.nvim'
+      "nvim-lua/plenary.nvim",
     },
-    opts = {}
+    opts = {},
   },
 
   -- Navigate and manipulate file system
   {
-    'stevearc/oil.nvim',
+    "stevearc/oil.nvim",
     dependencies = {
-      'nvim-tree/nvim-web-devicons',
+      "nvim-tree/nvim-web-devicons",
     },
     keys = {
-      { '-', '<cmd>Oil --float<cr>', { desc = 'Open parent directory' } },
+      { "-", "<cmd>Oil --float<cr>", { desc = "Open parent directory" } },
     },
     opts = {},
   },
@@ -71,60 +71,61 @@ return {
 
   -- Refactoring tool
   {
-    'ThePrimeagen/refactoring.nvim',
+    "ThePrimeagen/refactoring.nvim",
     keys = {
       {
-        '<leader>r',
+        "<leader>r",
         function()
-          require('refactoring').selector_refactor()
+          require("refactoring").selector_refactor()
         end,
-        desc = 'Refactoring tool',
-        mode = 'v',
+        desc = "Refactoring tool",
+        mode = "v",
       },
     },
-    opts = {}
+    opts = {},
   },
 
   -- Incremental rename
   {
-    'smjonas/inc-rename.nvim',
-    cmd = 'IncRename',
+    "smjonas/inc-rename.nvim",
+    cmd = "IncRename",
     config = true,
   },
 
   -- create annotations with one keybind, and jump your cursor in the inserted annotation
   {
-    'danymat/neogen',
+    "danymat/neogen",
     keys = {
       {
-        '<leader>cc',
+        "<leader>cc",
         function()
-          require('neogen').generate({})
+          require("neogen").generate({})
         end,
-        desc = 'Neogen Comment',
+        desc = "Neogen Comment",
       },
     },
-    opts = { snippet_engine = 'luasnip' },
+    opts = { snippet_engine = "luasnip" },
   },
 
   -- python venv selector
   -- refer: https://www.bilibili.com/video/BV1KD421p7Lv/?spm_id_from=333.1245.0.0&vd_source=857ae8fbced0ce6b196b0b0b931e3d87
   -- need create pyproject.toml in project dir
   {
-    'linux-cultist/venv-selector.nvim',
-    event = 'LazyFile',
+    "linux-cultist/venv-selector.nvim",
+    event = "LazyFile",
+    branch = "regexp",
     dependencies = {
-      'neovim/nvim-lspconfig',
-      'nvim-telescope/telescope.nvim',
-      'mfussenegger/nvim-dap-python',
+      "neovim/nvim-lspconfig",
+      "nvim-telescope/telescope.nvim",
+      "mfussenegger/nvim-dap-python",
     },
     keys = {
-      { '<leader>cv', '<cmd>VenvSelect<cr>' },
-      { '<leader>cV', '<cmd>VenvSelectCached<cr>' },
+      { "<leader>cv", "<cmd>VenvSelect<cr>" },
+      { "<leader>cV", "<cmd>VenvSelectCached<cr>" },
     },
     opts = {},
   },
 
   -- disable list
-  { 'folke/tokyonight.nvim', enable = false },
+  { "folke/tokyonight.nvim", enable = false },
 }
