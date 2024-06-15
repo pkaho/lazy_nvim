620772ca90ac0b9755cf508a1372326af70c5d3d
pkaho <pkaho@qq.com>
2024-06-15 23:19:49 +0800

    add neogro plugin

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,4 +1,15 @@
 return {
+  {
+    "vhyrro/luarocks.nvim",
+    priority = 1000,
+    config = true,
+  },
+  {
+    "nvim-neorg/neorg",
+    dependencies = { "luarocks.nvim" },
+    opts = {},
+  },
+
   -- project manager
   {
     "ahmedkhalf/project.nvim",
