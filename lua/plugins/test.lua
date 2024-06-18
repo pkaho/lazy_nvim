436078c3268d3e9454892767c73556237a31c237
pkaho <pkaho@qq.com>
2024-06-18 22:33:19 +0800

    add nvim-ufo plugin

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -1,4 +1,60 @@
 return {
+  {
+    "kevinhwang91/nvim-ufo",
+    dependencies = { "kevinhwang91/promise-async" },
+    event = "BufRead",
+    keys = {
+      {
+        "zR",
+        function()
+          require("ufo").openAllFolds()
+        end,
+        desc = "Open all folds",
+      },
+      {
+        "zM",
+        function()
+          require("ufo").closeAllFolds()
+        end,
+        desc = "Close all folds",
+      },
+      {
+        "zK",
+        function()
+          local winid = require("ufo").peekFoldedLinesUnderCursor()
+          if not winid then
+            vim.lsp.buf.hover()
+          end
+        end,
+        desc = "Peek folded lines under cursor",
+      },
+    },
+    config = function()
+      vim.o.foldcolumn = "1"
+      vim.o.foldlevel = 90
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+      require("ufo").setup({
+        open_fold_hl_timeout = 150,
+        close_fold_kinds_for_ft = {
+          default = { "imports", "comment" },
+        },
+        preview = {
+          win_config = {
+            border = { "", "─", "", "", "", "─", "", "" },
+            winhighlight = "Normal:Folded",
+            winblend = 0,
+          },
+          mappings = {
+            scrollU = "<C-u>",
+            scrollD = "<C-d>",
+            jumpTop = "[",
+            jumpBot = "]",
+          },
+        },
+      })
+    end,
+  },
   -- org file
   {
     "vhyrro/luarocks.nvim",
