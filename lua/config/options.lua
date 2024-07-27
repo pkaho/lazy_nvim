772377cc198eaca200d5b401cb03a1ae332d1f42
pkaho <pkaho@qq.com>
2024-07-27 16:21:36 +0800

    use basepyright

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,7 +5,7 @@
 local opt = vim.opt
 
 opt.inccommand = "split"
--- vim.g.lazyvim_python_lsp = "basepyright"
+vim.g.lazyvim_python_lsp = "basepyright"
 
 -- Check if "pwsh" is executable and set the shell accordingly
 -- refer: https://github.com/LazyVim/LazyVim/issues/2151#issuecomment-1888294994
