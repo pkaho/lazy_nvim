2c8296a048ce616433211179580566f5cbd3399b
pkaho <pkaho@qq.com>
2024-09-16 07:21:03 +0800

    use chezmoi extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -42,6 +42,7 @@ require("lazy").setup({
 
     { import = "lazyvim.plugins.extras.ui.edgy" },
     { import = "lazyvim.plugins.extras.ui.mini-indentscope" },
+    { import = "lazyvim.plugins.extras.util.chezmoi" },
     { import = "lazyvim.plugins.extras.util.project" },
 
     -- import/override with your plugins
