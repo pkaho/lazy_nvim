01d78e43eeb891162554847fe721e7ea6ab7f40b
pkaho <pkaho@qq.com>
2024-09-16 07:22:08 +0800

    dashboard rm logo

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,10 +1,19 @@
 return {
-  "nvimdev/dashboard-nvim",
-  opts = function(_, opts)
-    local logo = [[
+  {
+    "nvimdev/dashboard-nvim",
+    opts = function(_, opts)
+      local logo = [[
     ]]
-    logo = string.rep("\n", 8) .. logo .. "\n\n"
-    opts.config.header = vim.split(logo, "\n")
-    opts.theme = "doom"
-  end,
+      logo = string.rep("\n", 8) .. logo .. "\n\n"
+      opts.config.header = vim.split(logo, "\n")
+      opts.theme = "doom"
+    end,
+  },
+
+  {
+    "akinsho/bufferline.nvim",
+    keys = {
+      { "<leader>bc", "<cmd>BufferLinePick<cr>", "choice buffer" },
+    },
+  },
 }
