f72a4247d3ad19f60457cb21a9365d9519a7a420
pkaho <pkaho@qq.com>
2024-11-05 00:00:01 +0800

    add remote-nvim, overseer plugin

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -9,11 +9,28 @@ return {
   },
 
   {
-    "stevearc/overseer.nvim",
-    event = "VeryLazy",
-    opts = {},
+    "amitds1997/remote-nvim.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = true,
   },
 
+  {
+    "mbbill/undotree"
+  }
+
+  -- task runner and jot management plugin
+  -- {
+  --   "stevearc/overseer.nvim",
+  --   event = "VeryLazy",
+  --   opts = {},
+  -- },
+
+  -- like Cursor AI IDE
   -- {
   --   "yetone/avante.nvim",
   --   event = "VeryLazy",
