30827d12c90a218c58a3aa9be0441dfb761e2131
pkaho <pkaho@qq.com>
2024-11-20 23:23:40 +0800

    update lazyvim, rm dashboard.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,16 +1,5 @@
 return {
   {
-    "nvimdev/dashboard-nvim",
-    opts = function(_, opts)
-      local logo = [[
-    ]]
-      logo = string.rep("\n", 8) .. logo .. "\n\n"
-      opts.config.header = vim.split(logo, "\n")
-      opts.theme = "doom"
-    end,
-  },
-
-  {
     "akinsho/bufferline.nvim",
     keys = {
       { "<leader>bc", "<cmd>BufferLinePick<cr>", "choice buffer" },
